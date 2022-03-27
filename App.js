36b8bf528e2701f7915e74811efe343b4cd533b1
brokieb <brokieb@gmail.com>
2022-03-27 21:31:10 +0200

    upper menu

--- App.js
+++ App.js
@@ -1,62 +1,53 @@
-import {
-  faHouseMedical,
-  faUserPlus,
-} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Provider} from 'react-redux';
+import { faHouseMedical, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Provider } from "react-redux";
 
-import AddScreen from './src/screen/menuScreens/addContentMenuScreen';
-import ShowScreen from './src/screen/menuScreens/showContentMenuScreen';
-import store from './src/stores/store';
+import AddScreen from "./src/screen/menuScreens/addContentMenuScreen";
+import ShowScreen from "./src/screen/menuScreens/showContentMenuScreen";
+import { MenuContext } from "react-native-menu";
+import store from "./src/stores/store";
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-              let iconName;
+      <MenuContext style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
 
-              switch (route.name) {
-                case 'Przeglądaj':
-                  iconName = faUserPlus;
-                  break;
-                case 'Dodaj':
-                  iconName = faHouseMedical;
-                  break;
-              }
+                switch (route.name) {
+                  case "Przeglądaj":
+                    iconName = faUserPlus;
+                    break;
+                  case "Dodaj":
+                    iconName = faHouseMedical;
+                    break;
+                }
 
-              return <FontAwesomeIcon icon={iconName} size={24} />;
-            },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
-            headerShown: false,
-          })}
-        >
-          <Tab.Screen
-            name="Przeglądaj"
-            component={ShowScreen}
-            tabBarIcon={faUserPlus}
-          />
-          <Tab.Screen name="Dodaj" component={AddScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+                return <FontAwesomeIcon icon={iconName} size={24} />;
+              },
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "gray",
+              headerShown: false,
+            })}
+          >
+            <Tab.Screen
+              name="Przeglądaj"
+              component={ShowScreen}
+              tabBarIcon={faUserPlus}
+            />
+            <Tab.Screen name="Dodaj" component={AddScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </MenuContext>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/template/selectCardTodoMenuTemplate.js
+++ src/template/selectCardTodoMenuTemplate.js
@@ -1,18 +1,24 @@
-import {faWifi} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Text, TouchableOpacity, View} from 'react-native';
-import {Button} from 'react-native-elements';
+import { faWifi, faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Menu, {
+  MenuContext,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+} from "react-native-menu";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Button } from "react-native-elements";
 
-import {Style} from '../style/selectCardTodoTemplateStyle';
+import { Style } from "../style/selectCardTodoTemplateStyle";
 
 const Stack = createNativeStackNavigator();
-
-export const MainScreen = ({navigation, route, elements, title}) => {
+const renderTouchable = () => <TouchableOpacity />;
+export const MainScreen = ({ navigation, route, elements, title }) => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <View style={Style.container}>
-        {elements.map(({title, icon}, key) => (
+        {elements.map(({ title, icon }, key) => (
           <TouchableOpacity
             key={key}
             style={Style.singleBox}
@@ -29,20 +35,39 @@ export const MainScreen = ({navigation, route, elements, title}) => {
   );
 };
 
-const SelectCardTodoTemplate = ({screenTitle, screenName, elements}) => {
+const SelectCardTodoTemplate = ({ screenTitle, screenName, elements }) => {
   return (
     <Stack.Navigator>
       <Stack.Screen
         name={screenTitle}
         options={{
           headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              icon={<FontAwesomeIcon icon={faWifi} color="red" />}
-              disabled
-              type="clear"
-              color="yellow"
-            />
+            <>
+              <Button
+                icon={<FontAwesomeIcon icon={faWifi} color="red" size={20} />}
+                disabled
+                type="clear"
+                color="yellow"
+              />
+              <Menu
+                onSelect={(value) => alert(`User selected the number ${value}`)}
+              >
+                <MenuTrigger use>
+                  <FontAwesomeIcon icon={faBars} color="black" size={32} />
+                </MenuTrigger>
+                <MenuOptions style={{ paddingTop: 50 }}>
+                  <MenuOption value={1} renderTouchable={renderTouchable}>
+                    <Text>Moje konto</Text>
+                  </MenuOption>
+                  <MenuOption value={4} renderTouchable={renderTouchable}>
+                    <Text>Preferencje</Text>
+                  </MenuOption>
+                  <MenuOption value={5} renderTouchable={renderTouchable}>
+                    <Text>Wyloguj się</Text>
+                  </MenuOption>
+                </MenuOptions>
+              </Menu>
+            </>
           ),
         }}
       >
@@ -50,7 +75,7 @@ const SelectCardTodoTemplate = ({screenTitle, screenName, elements}) => {
           <MainScreen {...props} title={screenTitle} elements={elements} />
         )}
       </Stack.Screen>
-      {elements.map(({title, screen}, key) => (
+      {elements.map(({ title, screen }, key) => (
         <Stack.Screen name={title} component={screen} key={key} />
       ))}
     </Stack.Navigator>
